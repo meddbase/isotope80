0d37949c1a9ff3f9d1ee0599a5029020d738deb2
Paul Louth <plouth@meddbase.com>
2020-09-29 10:22:14 +0100

    Update logging unit test

--- samples/Isotope80.Samples.UnitTests/LoggingTests.cs
+++ samples/Isotope80.Samples.UnitTests/LoggingTests.cs
@@ -15,33 +15,36 @@ namespace Isotope80.Samples.UnitTests
         public void TestNestedContextualLogs()
         {
             Seq<string> expected = Seq(
-                "Test 1",
-                "Info for test 1",
-                "More info for test 1",
-                "\tTest 1.SubTest 1",
-                "\tInfo for test Test 1.SubTest 1",
-                "\tMore info for test Test 1.SubTest 1",
-                "\tTest 1.SubTest 2",
-                "\tInfo for test Test 1.SubTest 2",
-                "\tMore info for test Test 1.SubTest 2");
+                "Info log",
+                "\tTest 1",
+                "\tInfo for test 1",
+                "\tMore info for test 1",
+                "\t\tTest 1.SubTest 1",
+                "\t\tInfo for test Test 1.SubTest 1",
+                "\t\tMore info for test Test 1.SubTest 1",
+                "\t\tTest 1.SubTest 2",
+                "\t\tInfo for test Test 1.SubTest 2",
+                "\t\tMore info for test Test 1.SubTest 2");
             
             Seq<string> logs = default;
             
             var stgs = IsotopeSettings.Create();
             stgs.LogStream.Subscribe(x => logs = logs.Add(x.ToString()));
 
-            var iso2 = context("Test 1",
-                               from a in info("Info for test 1")
-                               from b in info("More info for test 1")
-                               from c in context("Test 1.SubTest 1",
-                                                 from d in info("Info for test Test 1.SubTest 1")
-                                                 from e in info("More info for test Test 1.SubTest 1")
-                                                 select unit)
-                               from f in context("Test 1.SubTest 2",
-                                                 from g in warn("Info for test Test 1.SubTest 2")
-                                                 from h in warn("More info for test Test 1.SubTest 2")
-                                                 select unit)
-                               select unit);
+            var iso2 = from _ in info("Info log")
+                       from r in context("Test 1",
+                                         from a in info("Info for test 1")
+                                         from b in info("More info for test 1")
+                                         from c in context("Test 1.SubTest 1",
+                                                           from d in info("Info for test Test 1.SubTest 1")
+                                                           from e in info("More info for test Test 1.SubTest 1")
+                                                           select unit)
+                                         from f in context("Test 1.SubTest 2",
+                                                           from g in warn("Info for test Test 1.SubTest 2")
+                                                           from h in warn("More info for test Test 1.SubTest 2")
+                                                           select unit)
+                                         select unit)
+                       select r;
                                               
                                 
             (var state2, var value2) = iso2.Run(stgs);
