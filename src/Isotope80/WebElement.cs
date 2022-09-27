e10c9a18e0b30a23b3ad0226511cf380c4b7d578
Tom Cole <tcole@meddbase.com>
2022-09-27 09:36:28 +0100

    Removing LangExt CodeGen and using C# records

--- src/Isotope80/WebElement.cs
+++ src/Isotope80/WebElement.cs
@@ -7,63 +7,28 @@ namespace Isotope80
     /// Immutable web-element that doesn't suffer the problems of the lazy IWebElement (namely if the IWebDriver is
     /// disposed then IWebElement fails, which is a problem for lazy processes).
     /// </summary>
-    [Record]
-    public partial class WebElement
+    /// <param name="Selector">SelectQuery selector that found this element</param>
+    /// <param name="SelectionIndex">This structure was made from a @select.  This is the index into the @select results</param>
+    /// <param name="Id">Element id attribute</param>
+    /// <param name="TagName">Element tag name</param>
+    /// <param name="Text">string Text</param>
+    /// <param name="Enabled">Element enabled flag</param>
+    /// <param name="Selected">Indicates whether or not this element is selected.</param>
+    /// <param name="Location">Gets a <see cref="T:System.Drawing.Point" /> object containing the coordinates of the upper-left corner of this element relative to the upper-left corner of the page.</param>
+    /// <param name="Size">Gets a <see cref="P:OpenQA.Selenium.IWebElement.Size" /> object containing the height and width of this element.</param>
+    /// <param name="Displayed">Indicates whether or not this element is displayed.</param>
+    public record WebElement(
+        Select Selector,
+        int SelectionIndex,
+        string Id,
+        string TagName,
+        string Text,
+        bool Enabled,
+        bool Selected,
+        Point Location,
+        Size Size,
+        bool Displayed)
     {
-        /// <summary>
-        /// SelectQuery selector that found this element
-        /// </summary>
-        public readonly Select Selector;
-        
-        /// <summary>
-        /// This structure was made from a @select.  This is the index into the @select results
-        /// </summary>
-        public readonly int SelectionIndex;
-        
-        /// <summary>
-        /// Element id attribute
-        /// </summary>
-        public readonly string Id;
-        
-        /// <summary>
-        /// Element tag name
-        /// </summary>
-        public readonly string TagName;
-        
-        /// <summary>
-        /// Element inner text
-        /// </summary>
-        public readonly string Text;
-
-        /// <summary>
-        /// Element enabled flag
-        /// </summary>
-        public readonly bool Enabled;
-
-        /// <summary>
-        /// Indicates whether or not this element is selected.
-        /// </summary>
-        public readonly bool Selected;
-
-        /// <summary>
-        /// Gets a <see cref="T:System.Drawing.Point" /> object containing the coordinates of the upper-left corner
-        /// of this element relative to the upper-left corner of the page.
-        /// </summary>
-        public readonly Point Location;
-
-        /// <summary>
-        /// Gets a <see cref="P:OpenQA.Selenium.IWebElement.Size" /> object containing the height and width of this element.
-        /// </summary>
-        public readonly Size Size;
-
-        /// <summary>
-        /// Indicates whether or not this element is displayed.
-        /// </summary>
-        public readonly bool Displayed;
-
-        /// <summary>
-        /// Compose the selector of the element and the @select
-        /// </summary>
         public static Select operator +(WebElement element, Select @select) =>
             element.Selector + @select;
     }
